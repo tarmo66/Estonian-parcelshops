b8233372c080f0ebfc396cf9ef7970ec66550fb5
tarmo66 <tarmo@traktorist.org>
2023-03-22 16:41:20 +0200

    pakiautomaadid portfolio

--- pakiautomaatid.php
+++ pakiautomaatid.php
@@ -7,34 +7,49 @@ Automaatide nimekirjad võetakse ettevõttete kodulehtedelt XML, JSON ja CSV for
 
 public function omnivaXML()
 {
-    $xml=simplexml_load_file("https://www.omniva.ee/locations.xml") or die("Error: Cannot create object");
 
-    foreach ($xml as $LOCATION)
-    {
-        $data = array(
-            'CARRIERID' => "OMNIVA",
-            'PACKOMATID' => substr(str_replace(' ', '', $LOCATION->NAME), 0, 10),
-            'COUNTRY' => substr($LOCATION->A0_NAME, 0, 10),
-            'ZIPCODEID' => substr($LOCATION->ZIP, 0, 10),
-            'STREET' => substr($LOCATION->A5_NAME, 0, 138) . " " . substr($LOCATION->A7_NAME, 0, 10) . " " .substr($LOCATION->A8_NAME, 0, 10),
-            'CITY' => substr($LOCATION->A2_NAME, 0, 60),
-            'ADDRESS' => substr($LOCATION->A1_NAME, 0, 80) . " " . substr($LOCATION->A2_NAME, 0, 80) . " " . substr($LOCATION->A3_NAME, 0, 88),
-            'NAME' => substr($LOCATION->NAME, 0, 160),
-            'LONGITUDE' => $LOCATION->X_COORDINATE,
-            'LATITUDE' => $LOCATION->Y_COORDINATE
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://www.omniva.ee/locations.csv");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $body = curl_exec($ch);
+    curl_close($ch);
+
+    $body = str_getcsv($body,"\n");
+
+    //var_dump($body);
+    foreach ($body as $bodyElement) {
+        $bodyCsv = explode(';', $bodyElement);
+        if ($bodyCsv[11] != NULL) {
+            $flatNumber = '-' . trim(str_replace('"', '', $bodyCsv[11]));
+        } else {
+            $flatNumber = '';
+        }
+        if ($bodyCsv[5] != NULL) {
+            $city = trim(str_replace('"', '', $bodyCsv[5])) . ', ';
+        } else {
+            $city = '';
+        }
+        $insert = array(
+            'CARRIERID'=>($bodyCsv[2] == 0 ? 'OMNIVA_PAC' : 'OMNIVAPOST'),
+            'PACKOMATID'=>trim(str_replace('"', '', $bodyCsv[0])),
+            'COUNTRY'=>trim(str_replace('"', '', $bodyCsv[3])),
+            'ZIPCODEID'=>trim(str_replace('"', '', $bodyCsv[0])),
+            'STREET'=>trim(str_replace('"', '', $bodyCsv[8])) . ' ' . trim(str_replace('"', '', $bodyCsv[10])) . $flatNumber,
+            'CITY'=>( strlen(trim(str_replace('"', '', $bodyCsv[5])))>0 ? trim(str_replace('"', '', $bodyCsv[5])) : trim(str_replace('"', '', $bodyCsv[4])) ),
+            'ADDRESS'=>trim(str_replace('"', '', $bodyCsv[4])),
+            'NAME'=>trim(str_replace('"', '', $bodyCsv[1])) . ', ' . trim(str_replace('"', '', $bodyCsv[8])) . ' ' . trim(str_replace('"', '', $bodyCsv[10])) . $flatNumber, 
+            'LONGITUDE'=>str_replace('Longitude', '', trim(str_replace('"', '', $bodyCsv[12]))),
+            'LATITUDE'=>trim(str_replace('"', '', $bodyCsv[13]))
         );
-        $this->db->insert('asukohad', $data);
+        $this->andmeladu_model->insertOrUpdate('WEB.dbo.web_MecCarrierPackomats',$insert,array('COUNTRY'=>$insert['COUNTRY'],'CARRIERID'=>$insert['CARRIERID'],'PACKOMATID'=>$insert['PACKOMATID']));
     }
-
-    $xml = "";
-    $data = "";
 }
 
 public function smartpostLae($filename)
 {
     $jsonData = file_get_contents($filename);
     $jsonOk = substr($jsonData, 13, -1);
-    //echo $jsonOk;
     $json = json_decode($jsonOk);
     
     foreach ($json as $item) 
@@ -51,9 +66,7 @@ public function smartpostLae($filename)
             'LONGITUDE' => $item->location->lon,
             'LATITUDE' => $item->location->lat
         );
-        //echo $item->id;
-        //echo $item->publicName->fi;
-        //echo $item->longitude;
+
         $this->db->insert('asukohad', $data);
     }
 
@@ -82,86 +95,39 @@ public function smartpostJSON()
     $this->smartpostLae($filename);
 }
 
-public function dpdCSV()
-{
-    $lines =file('ftp://ftp.dpd.ee/parcelshop/psexport_latest.csv');
-    array_shift($lines);
-    $x = 0;
-
-    foreach($lines as $data)
-    {
-        list(
-            $paketshop[],
-            $depot[],
-            $firma[],
-            $aadress[],
-            $indeks[],
-            $linn[],
-            $notused7[],
-            $notused8[],
-            $notused9[],
-            $notused10[],
-            $notused11[],
-            $notused12[],
-            $notused13[],
-            $notused14[],
-            $notused15[],
-            $notused16[],
-            $notused17[],
-            $notused18[],
-            $notused19[],
-            $notused20[],
-            $notused21[],
-            $notused22[],
-            $country[]
-        )
-        = explode('|',$data);
-        $data = array(
-            'CARRIERID' => "DPD",
-            'PACKOMATID' => substr($paketshop[$x], 0, 10),
-            'COUNTRY' => substr($country[$x], 0, 2),
-            'ZIPCODEID' => substr($indeks[$x], 0, 10),
-            'STREET' => substr($aadress[$x], 0, 160),
-            'CITY' => substr($linn[$x], 0, 60),
-            //'ADDRESS' => "",
-            'NAME' => substr($firma[$x], 0, 160)
-            //'LONGITUDE' => "",
-            //'LATITUDE' => ""
-        );
-
-        $this->db->insert('asukohad', $data);
-        $x++;
-    }
-}
 
 public function matkahuoltoJSON()
 {
-    //$jsonobj = file("https://www.api.matkahuolto.io/search/offices/area?left=11.62348034419147&right=42.003271016759065&top=70.93414296843171&bottom=59.07655569867418");
-    //$filename = 'http://traktorist.org/stuff/jsontest.json';
-    $filename = 'https://www.api.matkahuolto.io/search/offices/area?left=11.62348034419147&right=42.003271016759065&top=70.93414296843171&bottom=59.07655569867418';
 
-    $jsonData = file_get_contents($filename);
-    $json = json_decode($jsonData);
+    $url='https://www.api.matkahuolto.io/search/offices/area?left=11.62348034419147&right=42.003271016759065&top=70.93414296843171&bottom=59.07655569867418';
 
-    $x = 0;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $result = curl_exec($ch);
+    curl_close($ch);  
+    $result=json_decode($result, true);
 
-    foreach ($json as $item) 
-    {
-        $data = array(
-            'CARRIERID' => "MATKAHUOLT",
-            'PACKOMATID' => $item->officeCode,
-            'COUNTRY' => "FI",
-            'ZIPCODEID' => $item->officePostalCode,
-            'STREET' => substr($item->officeStreetAddress, 0, 160),
-            'CITY' => substr($item->officeCity, 0, 60),
-            'ADDRESS' => "",
-            'NAME' => substr($item->officeName, 0, 160),
-            'LONGITUDE' => $item->longitude,
-            'LATITUDE' => $item->latitude
-        ); 
-        $this->db->insert('asukohad', $data);
-        $x++;
+    if( count($result)>0 ){
+        foreach ($result as $kk => $vv) {
+            $insert =array(
+                'CARRIERID'=>'MATKAHUOLT',  
+                'PACKOMATID'=>$vv['officeCode'],
+                'COUNTRY'=>'FI',
+                'ZIPCODEID'=>$vv['officePostalCode'],
+                'STREET'=>$vv['officeStreetAddress'],
+                'CITY'=>$vv['officeCity'],
+                'ADDRESS'=>$vv['officeStreetAddress'],
+                'NAME'=>mb_convert_encoding($vv['officeCity'] . ', ' .$vv['officeName'] . ', ' . $vv['officeStreetAddress'], 'HTML-ENTITIES', "UTF-8"),
+                'LONGITUDE'=>$vv['longitude'],
+                'LATITUDE'=>$vv['latitude'],
+            );
+
+            $this->andmeladu_model->insertOrUpdate('WEB.dbo.web_MecCarrierPackomats',$insert,array('COUNTRY'=>$insert['COUNTRY'],'CARRIERID'=>$insert['CARRIERID'],'PACKOMATID'=>$insert['PACKOMATID']));
+        }
     }
+
 }
 
 ?>
